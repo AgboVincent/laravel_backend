b1162c829f6afa5c719f56327bb345507e83af18
Lawal Oladipupo Olawunmi <lawaloladipupo@outlook.com>
2021-07-22 07:05:35 +0100

    Fixed issue with php stan

--- app/ModelFilters/ClaimFilter.php
+++ app/ModelFilters/ClaimFilter.php
@@ -6,12 +6,12 @@ use EloquentFilter\ModelFilter;
 
 class ClaimFilter extends ModelFilter
 {
-    public function policy(string $policy): self
+    public function policy($policy)
     {
-        return $this->related('policy', 'number', '=', $policy);
+        return $this->related('policy', 'number', $policy);
     }
 
-    public function status(string $status): self
+    public function status(string $status)
     {
         return $this->where('status', $status);
     }
