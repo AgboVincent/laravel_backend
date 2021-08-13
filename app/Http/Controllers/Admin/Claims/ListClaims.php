8f3e890e4dd06e3b88559f175da84a88e3873e15
Lawal Oladipupo Olawunmi <lawaloladipupo@outlook.com>
2021-08-13 23:29:32 +0100

    Added filter to claims list for broker

--- app/Http/Controllers/Admin/Claims/ListClaims.php
+++ app/Http/Controllers/Admin/Claims/ListClaims.php
@@ -7,6 +7,7 @@ use App\Helpers\Output;
 use App\Http\Controllers\Controller;
 use App\Http\Resources\Admin\ClaimResource;
 use App\Http\Resources\PaginatedResource;
+use App\Models\User;
 use Illuminate\Http\JsonResponse;
 use Illuminate\Http\Request;
 
@@ -14,11 +15,17 @@ class ListClaims extends Controller
 {
     public function __invoke(Request $request): JsonResponse
     {
+        $query = Auth::user()->company->claims()->filter($request->all());
+
+        if (Auth::user()->type === User::TYPE_BROKER) {
+            $query = $query
+                ->join('users', 'users.id', '=', 'policies.user_id')
+                ->whereRaw('JSON_CONTAINS(JSON_UNQUOTE(meta), ?, ?)', [Auth::user()->id, '$.broker_id']);
+        }
+
         return Output::success(
             new PaginatedResource(
-                Auth::user()->company->claims()
-                    ->filter($request->all())
-                    ->paginate(),
+                $query->paginate(),
                 ClaimResource::class
             )
         );
