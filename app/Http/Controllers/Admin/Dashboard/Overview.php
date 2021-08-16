fc6771c403635efd7a7cee9e1da964cb63918ddd
Lawal Oladipupo Olawunmi <lawaloladipupo@outlook.com>
2021-08-16 15:44:45 +0100

    Fixed with dashboard not including filter for broker

--- app/Http/Controllers/Admin/Dashboard/Overview.php
+++ app/Http/Controllers/Admin/Dashboard/Overview.php
@@ -7,17 +7,27 @@ use App\Helpers\Output;
 use App\Http\Controllers\Controller;
 use App\Models\Claim;
 use App\Models\ClaimItem;
+use App\Models\User;
 use Illuminate\Http\JsonResponse;
 
 class Overview extends Controller
 {
     public function __invoke(): JsonResponse
     {
+        if (Auth::user()->type === User::TYPE_BROKER) {
+            $builder = Claim::query()
+                ->join('policies', 'policies.id', '=', 'claims.policy_id')
+                ->join('users', 'users.id', '=', 'policies.user_id')
+                ->whereRaw('JSON_CONTAINS(JSON_UNQUOTE(meta), ?, ?)', [Auth::user()->id, '$.broker_id']);
+        } else {
+            $builder = Auth::user()->company->claims();
+        }
+
         return Output::success([
-            'total_claims' => Auth::user()->company->claims()->count(),
-            'processed_claims' => Auth::user()->company->claims()->where('claims.status', Claim::STATUS_APPROVED)->count(),
-            'pending_claims' => Auth::user()->company->claims()->where('claims.status', Claim::STATUS_PENDING)->count(),
-            'claims_value' => Auth::user()->company->claims()
+            'total_claims' => $builder->count(),
+            'processed_claims' => ($builder->clone())->where('claims.status', Claim::STATUS_APPROVED)->count(),
+            'pending_claims' => ($builder->clone())->where('claims.status', Claim::STATUS_PENDING)->count(),
+            'claims_value' => ($builder->clone())
                 ->join('accidents', 'accidents.claim_id', '=', 'claims.id')
                 ->join('claim_items', 'claim_items.accident_id', '=', 'accidents.id')
                 ->where('claim_items.status', ClaimItem::STATUS_APPROVED)
