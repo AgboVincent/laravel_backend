1ecb7a1d1fb0e7c062c6289d69810f2c6b85172a
Lawal Oladipupo Olawunmi <lawaloladipupo@outlook.com>
2021-08-16 15:48:25 +0100

    Fixed linting issue

--- app/Http/Controllers/Claims/Comments/AddComment.php
+++ app/Http/Controllers/Claims/Comments/AddComment.php
@@ -7,12 +7,16 @@ use App\Http\Controllers\Controller;
 use App\Http\Requests\Claim\Comment\CreateRequest;
 use App\Http\Resources\CommentResource;
 use App\Models\Claim;
+use App\Models\Comment;
 use Illuminate\Http\JsonResponse;
 
 class AddComment extends Controller
 {
     public function __invoke(Claim $claim, CreateRequest $request): JsonResponse
     {
+        /**
+         * @var Comment $comment
+         */
         $comment = $claim->comment($request->get('comment'));
 
         //todo: notify those needed to be notified.
