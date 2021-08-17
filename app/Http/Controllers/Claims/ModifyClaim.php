74a75fe3f4a62afc1220708680f7e7e6ce9fcaeb
Lawal Oladipupo Olawunmi <lawaloladipupo@outlook.com>
2021-08-17 14:23:28 +0100

    Fixed issue with date for updating claim

--- app/Http/Controllers/Claims/ModifyClaim.php
+++ app/Http/Controllers/Claims/ModifyClaim.php
@@ -8,13 +8,14 @@ use App\Http\Requests\Claim\ModifyRequest;
 use App\Http\Resources\Admin\ClaimResource;
 use App\Models\Claim;
 use Illuminate\Http\JsonResponse;
+use Illuminate\Support\Carbon;
 
 class ModifyClaim extends Controller
 {
     public function __invoke(Claim $claim, ModifyRequest $request): JsonResponse
     {
         $claim->accident->update([
-            'occurred_at' => $request->get('date_time'),
+            'occurred_at' => Carbon::parse($request->get('date_time')),
             'accident_type_id' => $request->get('accident_type'),
             'description' => $request->get('description'),
             'involved_third_party' => (bool)$request->get('involved_third_party')
