a6bf0c340e7b98f7011300cadcdb798724592aed
Lawal Oladipupo Olawunmi <lawaloladipupo@outlook.com>
2021-09-10 11:01:04 +0100

    Completed

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,6 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
-use Kreait\Laravel\Firebase\Facades\Firebase;
 
 /*
 |--------------------------------------------------------------------------
@@ -15,7 +14,6 @@ use Kreait\Laravel\Firebase\Facades\Firebase;
 */
 
 Route::get('/', function () {
-        \App\Models\Claim::find(8)->comment('Here is where you can register web routes for your application. These routes are loaded by the RouteServiceProvider within a group which');
     return view('welcome');
 });
 
