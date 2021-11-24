684eb38ad17397d7edba1143e520670465c68c0e
Temitayo Ajayi <temmyjay@curacel.ai>
2021-11-24 11:52:42 +0100

    AP-14: Pass uploaded image through image classification model

--- app/Jobs/ProcessClaimImagesToML.php
+++ app/Jobs/ProcessClaimImagesToML.php
@@ -23,40 +23,59 @@ class ProcessClaimImagesToML implements ShouldQueue
     /**
      * @var Upload[]|Collection
      */
-    private $images;
+    private $image;
 
     public function __construct(Claim $claim)
     {
         $this->claim = $claim;
-        $this->images = $this->claim->accident->media;
+        $this->image = $this->claim->accident->media;
     }
 
     public function handle()
     {
-        $request = Http::post('https://pmm2k4a73j.execute-api.us-east-2.amazonaws.com/test/estimates', [
-            'images' => [
-                'front' => $this->images->where('type', AccidentMedia::TYPE_FRONT)->first(),
-                'back' => $this->images->where('type', AccidentMedia::TYPE_REAR)->first(),
-                'close-up' => $this->images->where('type', AccidentMedia::TYPE_CLOSE_UP)->first(),
-            ]
+//        'https://pmm2k4a73j.execute-api.us-east-2.amazonaws.com/test/estimates'
+        $objectDetectionService = 'https://curacel-vehicle-report.herokuapp.com/report';
+
+        $imageClassificationService = "https://24lf5b0s7j.execute-api.us-east-2.amazonaws.com/production/vehicleAccidentClassification";
+        $uploadId = $this->image->where('type', AccidentMedia::TYPE_CLOSE_UP)->first()->upload_id;
+        $imagePath = (new \App\Models\Upload)->find($uploadId)->path;
+
+        $imageClassificationRequest = Http::post($imageClassificationService, [
+            'image_data' => $imagePath
         ]);
 
-        if ($request->failed()) {
-            Log::debug('Claim Machine Learning Process Failed with Status: ' . $request->status());
-            Log::debug($request->body());
-            return;
+        if($imageClassificationRequest->failed()) {
+            Log::debug('Image classification model process failed with Status: ' . $imageClassificationRequest->status());
+            Log::debug($imageClassificationRequest->body());
+        } else {
+            if($imageClassificationRequest->json()['is_damaged']) {
+                $objectDetectionServiceRequest = Http::post($objectDetectionService, [
+                    'image' => (new \App\Models\Upload)->find($uploadId)->path,
+                    'model' => $this->claim->policy->vehicle->model,
+                    'year' => $this->claim->policy->vehicle->year
+                ]);
+
+                if ($objectDetectionServiceRequest->failed()) {
+                    Log::debug('Claim Machine Learning Process Failed with Status: ' . $objectDetectionServiceRequest->status());
+                    Log::debug($objectDetectionServiceRequest->body());
+                    return;
+                }
+
+                $data = collect($objectDetectionServiceRequest->json()['detected_damages']);
+
+                $data->each(function ($result) {
+                    if (isset($result['pred_boxes'])) {
+                        $type = ClaimItemType::query()->where('key', $result['damage'])->first();
+
+                        if ($type) {
+                            $this->claim->items()->where('type_id', $type->id)->update([
+                                'amount' => $result['price']
+                            ]);
+                        }
+                    }
+                });
+            }
         }
 
-        $data = collect(json_decode($request->body(), true)['detected_damages']);
-
-        $data->each(function ($result) {
-            $type = ClaimItemType::query()->where('name', $result['damage'])->first();
-
-            if ($type) {
-                $this->claim->items()->where('type_id', $type->id)->update([
-                    'amount' => $result['price']
-                ]);
-            }
-        });
     }
 }
