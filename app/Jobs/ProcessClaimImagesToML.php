1b6b9781d2a3949f38fa60f47d79583ac69e69ba
Temitayo Ajayi <temmyjay@curacel.ai>
2021-12-01 09:44:19 +0100

    AP-27: add ml results to data base

--- app/Jobs/ProcessClaimImagesToML.php
+++ app/Jobs/ProcessClaimImagesToML.php
@@ -2,6 +2,7 @@
 
 namespace App\Jobs;
 
+use App\Actions\ClassifyImage;
 use App\Models\AccidentMedia;
 use App\Models\Claim;
 use App\Models\ClaimItemType;
@@ -35,52 +36,59 @@ class ProcessClaimImagesToML implements ShouldQueue
     {
 //        'https://pmm2k4a73j.execute-api.us-east-2.amazonaws.com/test/estimates'
         $objectDetectionService = 'https://curacel-vehicle-report.herokuapp.com/report';
-
-        $imageClassificationService = "https://24lf5b0s7j.execute-api.us-east-2.amazonaws.com/production/vehicleAccidentClassification";
         $uploadId = $this->image->where('type', AccidentMedia::TYPE_CLOSE_UP)->first()->upload_id;
         $imagePath = (new \App\Models\Upload)->find($uploadId)->path;
 
-        Log::info('Image Path', [$imagePath]);
+        $isVehicleDamaged = ClassifyImage::run($imagePath);
 
-        $imageClassificationRequest = Http::post($imageClassificationService, [
-            'image_data' => $imagePath
-        ]);
+        if ($isVehicleDamaged) {
+            Log::info('Image classification model classified vehicle as damaged');
+            $objectDetectionServiceRequest = Http::post($objectDetectionService, [
+                'image' => $imagePath,
+                'model' => $this->claim->policy->vehicle->model,
+                'year' => $this->claim->policy->vehicle->year
+            ]);
 
-        if($imageClassificationRequest->failed()) {
-            Log::debug('Image classification model process failed with Status: ' . $imageClassificationRequest->status());
-            Log::debug($imageClassificationRequest->body());
-        } else {
-            Log::info('Image classification ok');
-            if($imageClassificationRequest->json()['is_damaged']) {
-                Log::info('Image classification model classified vehicle as damaged');
-                $objectDetectionServiceRequest = Http::post($objectDetectionService, [
-                    'image' => $imagePath,
-                    'model' => $this->claim->policy->vehicle->model,
-                    'year' => $this->claim->policy->vehicle->year
-                ]);
+            if ($objectDetectionServiceRequest->failed()) {
+                Log::debug('Claim Machine Learning Process Failed with Status: ' . $objectDetectionServiceRequest->status());
+                Log::debug($objectDetectionServiceRequest->body());
+                return;
+            }
 
-                if ($objectDetectionServiceRequest->failed()) {
-                    Log::debug('Claim Machine Learning Process Failed with Status: ' . $objectDetectionServiceRequest->status());
-                    Log::debug($objectDetectionServiceRequest->body());
-                    return;
-                }
+            $data = collect($objectDetectionServiceRequest->json()['detected_damages']);
+            Log::info('Object detection model ran successfully and got this result', [$data]);
 
-                $data = collect($objectDetectionServiceRequest->json()['detected_damages']);
-                Log::info('Object detection model ran successfully and got this result', [$data]);
+            $data->each(function ($result) {
+                if (isset($result['pred_boxes'])) {
+                    $type = ClaimItemType::query()->where('key', $result['damage'])->first();
 
-                $data->each(function ($result) {
-                    if (isset($result['pred_boxes'])) {
-                        $type = ClaimItemType::query()->where('key', $result['damage'])->first();
-
-                        if ($type) {
-                            $this->claim->items()->where('type_id', $type->id)->update([
+                    if ($type) {
+                        $claimItemType = $this->claim->items()->where('type_id', $type->id)->first();
+                        if ($claimItemType) {
+                            $claimItemType->update([
                                 'amount' => $result['price']
                             ]);
+                        } else {
+                            $this->claim->items()->create([
+                                'accident_id' => $this->claim->accident->id,
+                                'type_id' => $type->id,
+                                'quantity' => 1,
+                                'amount' => $result['price'],
+                                'ml_prediction' => 1
+                            ]);
                         }
+                    } else {
+                        $newClaimType = ClaimItemType::create(['name' => ucfirst(str_replace('_', " ", $result['damage'])), 'key' => $result['damage']]);
+                        $this->claim->items()->create([
+                            'accident_id' => $this->claim->accident->id,
+                            'type_id' => $newClaimType->id,
+                            'quantity' => 1,
+                            'amount' => $result['price'],
+                            'ml_prediction' => 1
+                        ]);
                     }
-                });
-            }
+                }
+            });
         }
-
     }
 }
