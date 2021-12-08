6f4e09dc2fbb0c14934fdc1bc38ff06e3d051bdd
Emeka Adirije <emeka.a@curacel.ai>
2021-12-08 13:34:15 +0100

    AP-38: create handlers for the list-claims endpoint

---
+++ app/Actions/Integrations/Baloon/GetClaimsListURL.php
@@ -0,0 +1,67 @@
+<?php
+
+namespace App\Actions\Integrations\Baloon;
+
+use App\Helpers\JWT;
+use App\Models\User;
+use App\Helpers\Output;
+use App\Helpers\Integrations\Baloon;
+use App\Http\Resources\LoginResource;
+use Lorisleiva\Actions\Concerns\AsAction;
+use App\Http\Requests\Integrations\Baloon\ListClaimsURLRequest;
+
+/**
+ * @mixin AsAction
+ */
+class GetClaimsListURL
+{   
+    use AsAction;
+
+    public function handle(array $requestPayload)
+    {
+        $jwtPayload = JWT::decodePayload($requestPayload['baloonSsoInfo']['token']);
+
+        $user = Baloon::createUser($jwtPayload);
+
+        $data = [];
+
+        $data['url'] = $this->getClaimsURL($user);
+        $data['token'] = $this->getCuracelAuthToken($user);
+
+        // always include Baloon's SSO info
+        $data['baloonSsoInfo'] = $requestPayload['baloonSsoInfo'];
+
+        return $data;
+
+    }
+
+    public function AsController(ListClaimsURLRequest $request)
+    {
+        $data = $this->handle($request->all());
+       
+        return Output::success($data);
+
+    }
+
+    /**
+     * Get the bearer token for authentication on redirect.
+     * 
+     * @param  User $user
+     * @return string
+     */
+    protected function getCuracelAuthToken(User $user)
+    {
+        return (new LoginResource($user))->toArray(null)['token'];
+    }
+
+    /**
+     * Get the list-claims URL for the user.
+     * 
+     * @param  User  $user
+     * @return string
+     */
+    protected function getClaimsURL(User $user)
+    {
+        return \config('app.front') . "claims";;
+    }
+}
