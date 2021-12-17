9e49febd4da5655fc00a5eaf815f6510754c4182
John Flash <Johngood4real@yahoo.co.in>
2021-12-17 02:59:55 +0100

    AP-70 Add claim relationship to finacial movements

--- app/Models/ClaimFinancialMovement.php
+++ app/Models/ClaimFinancialMovement.php
@@ -8,4 +8,8 @@ use Illuminate\Database\Eloquent\Model;
 class ClaimFinancialMovement extends Model
 {
     use HasFactory;
+
+    public function claim(){
+        return $this->belongsTo(Claim::class);
+    }
 }
