ff1ef0db00209d048315c1fabbf4ff91c9b52f61
Emeka Adirije <emeka.a@curacel.ai>
2021-12-17 15:23:31 +0100

    update expert reports file path to public

--- app/Models/ExpertReport.php
+++ app/Models/ExpertReport.php
@@ -2,12 +2,25 @@
 
 namespace App\Models;
 
-use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Support\Facades\Storage;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
 
 class ExpertReport extends Model
 {
     use HasFactory;
 
     protected $guarded = ['id'];
+
+    protected $appends = ['file'];
+    // protected $hidden = ['file_path'];
+
+    public function getFileAttribute(): string
+    {
+        if(env('APP_ENV') == 'local') {
+            return Storage::disk('uploads')->url($this->file_path);
+        } else {
+            return Storage::disk('s3')->url($this->file_path);
+        }
+    }
 }
