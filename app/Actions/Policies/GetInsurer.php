e46e66856a6425762e6f4ead18f4ef0c20c184f1
Temitayo Ajayi <temmyjay@curacel.ai>
2021-12-20 12:36:04 +0100

    Add as Action to get insurer class

--- app/Actions/Policies/GetInsurer.php
+++ app/Actions/Policies/GetInsurer.php
@@ -4,9 +4,11 @@ namespace App\Actions\Policies;
 
 use App\Models\Policy;
 use App\Helpers\Output;
+use Lorisleiva\Actions\Concerns\AsAction;
 
 class GetInsurer
 {
+    use AsAction;
     public function handle(Policy $policy)
     {
         return $policy->insurer;
