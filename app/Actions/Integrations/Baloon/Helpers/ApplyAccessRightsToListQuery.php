ec86471c95f9cc65b22db7beab1d16b49a8a7bbb
John Flash <Johngood4real@yahoo.co.in>
2021-12-20 06:58:55 +0100

    AP-100 Make ApplyAccessRightsToListQuery compatible for claims queries
    - Move the filter generation closure into separate method for room cleaning.
    - Add a queryType parameter to ApplyAccessRightsToListQuery to determine which model is being queried and set it to a default of policy
    - Use the queryType to influence how the query filters should be built.

--- app/Actions/Integrations/Baloon/Helpers/ApplyAccessRightsToListQuery.php
+++ app/Actions/Integrations/Baloon/Helpers/ApplyAccessRightsToListQuery.php
@@ -2,6 +2,7 @@
 
 namespace App\Actions\Integrations\Baloon\Helpers;
 
+use App\DTOs\Integrations\Baloon\AccessRight;
 use App\DTOs\Integrations\Baloon\AccessRightCollection;
 use App\Helpers\Integrations\Baloon;
 use App\Models\User;
@@ -16,8 +17,10 @@ class ApplyAccessRightsToListQuery
     const QUERY_TYPE_POLICY = 'policy';
     const QUERY_TYPE_CLAIM = 'claim';
 
-    public function handle(User $user, $query)
+    public function handle(User $user, $query, string $queryType=self::QUERY_TYPE_POLICY)
     {
+        $this->queryType = $queryType;
+
         $accessRights = $this->loadAccessRights($user);
         if($accessRights){
             $query = $this->buildFilters($accessRights,$query);
@@ -26,7 +29,7 @@ class ApplyAccessRightsToListQuery
         return $query;
     }
 
-    //TODO: reduce complexity
+
     protected function buildFilters(AccessRightCollection $accessRights, $query){
 
         $query->where(function ($query) use ($accessRights){
@@ -40,34 +43,47 @@ class ApplyAccessRightsToListQuery
                     continue;
                 }
 
-                $query->orWhere(function($query) use ($accessRight){
-                    if($accessRight->compagnies){
-                        $insurerIds = Baloon::getInsurerIdsForCompagnies($accessRight->compagnies);
-
-                        $query->whereHas('insurer',function ($insurers) use ($insurerIds){
-                            $insurers->whereIn('insurers.id',$insurerIds);
-                        });
-                    }
-
-                    if(count($accessRight->reseaux)){
-                        $query->whereHas('metas',function ($metas) use ($accessRight){
-                            $metas->where('name',Baloon::META_KEY_RESEAU_ID)
-                                ->whereIn('value',$accessRight->reseaux);
-                        });
-                    }
-
-                    if(count($accessRight->acteursCommerciaux)){
-                        $query->whereHas('metas',function ($metas) use ($accessRight){
-                            $metas->where('name',Baloon::META_KEY_ACTEUR_COMMERCIAL_ID)
-                                ->whereIn('value',$accessRight->acteursCommerciaux);
-                        });
-                    }
-                });
-
+                $query->orWhere($this->makeFilterClosure($accessRight));
             }
         });
 
         return $query;
+    }
+
+    protected function makeFilterClosure(AccessRight $accessRight):\Closure{
+        return function($query) use ($accessRight){
+            if($accessRight->compagnies){
+                $insurerIds = Baloon::getInsurerIdsForCompagnies($accessRight->compagnies);
+
+                $relationName = 'insurer';
+                if($this->queryType==self::QUERY_TYPE_CLAIM){
+                    $relationName = 'policy.insurer';
+                }
+
+                $query->whereHas($relationName,function ($insurers) use ($insurerIds){
+                    $insurers->whereIn('insurers.id',$insurerIds);
+                });
+            }
+
+            $metaRelationName = 'metas';
+            if($this->queryType==self::QUERY_TYPE_CLAIM){
+                $metaRelationName = 'policy.metas';
+            }
+
+            if(count($accessRight->reseaux)){
+                $query->whereHas($metaRelationName,function ($metas) use ($accessRight){
+                    $metas->where('name',Baloon::META_KEY_RESEAU_ID)
+                        ->whereIn('value',$accessRight->reseaux);
+                });
+            }
+
+            if(count($accessRight->acteursCommerciaux)){
+                $query->whereHas($metaRelationName,function ($metas) use ($accessRight){
+                    $metas->where('name',Baloon::META_KEY_ACTEUR_COMMERCIAL_ID)
+                        ->whereIn('value',$accessRight->acteursCommerciaux);
+                });
+            }
+        };
     }
 
 }
