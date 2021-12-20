96db00a7cbe6d367362005c69302d228d62ee6d1
John Flash <Johngood4real@yahoo.co.in>
2021-12-20 08:16:14 +0100

    AP-100 Modify claim list endpoint
    - Remove excess relations from being fetched
    - Add condition to fetch the claims via the user's owner with backward compatibility to fallback on the old query.
    - Add baloon acessRight filters to the query

--- app/Http/Controllers/Admin/Claims/ListClaims.php
+++ app/Http/Controllers/Admin/Claims/ListClaims.php
@@ -2,10 +2,13 @@
 
 namespace App\Http\Controllers\Admin\Claims;
 
+use App\Actions\Integrations\Baloon\Helpers\ApplyAccessRightsToListQuery;
 use App\Helpers\Auth;
+use App\Helpers\Integrations\Baloon;
 use App\Helpers\Output;
 use App\Http\Controllers\Controller;
 use App\Http\Resources\Admin\ClaimResource;
+use App\Http\Resources\ClaimListResource;
 use App\Http\Resources\PaginatedResource;
 use App\Models\User;
 use Illuminate\Http\JsonResponse;
@@ -15,6 +18,41 @@ class ListClaims extends Controller
 {
     public function __invoke(Request $request): JsonResponse
     {
+
+        $query = $this->makeQuery();
+
+        return Output::success(
+            new PaginatedResource(
+                $query->filter($request->all())->with([
+                    'policy.vehicle', 'user:users.id,first_name,last_name'
+                ])->paginate(),
+                ClaimListResource::class
+            )
+        );
+    }
+
+    protected function makeQuery(){
+        $user = Auth::user();
+
+        if($user->owner){
+            $query = $user->owner->claims();
+            return $this->applyMoreFilters($user,$query);
+        }
+
+        return $this->oldQuery();
+
+    }
+
+    protected function applyMoreFilters(User $user, $query){
+        if($user->owner->code==Baloon::BROKER_CODE){
+            $query = ApplyAccessRightsToListQuery::make()
+                ->handle($user,$query,ApplyAccessRightsToListQuery::QUERY_TYPE_CLAIM);
+        }
+
+        return $query;
+    }
+
+    protected function oldQuery(){
         $query = Auth::user()->company->claims()->latest('claims.created_at');
 
         if (Auth::user()->type === User::TYPE_BROKER) {
@@ -26,14 +64,6 @@ class ListClaims extends Controller
             $query = $query->where('involves_insurer', true);
         }
 
-        return Output::success(
-            new PaginatedResource(
-                $query->filter($request->all())->with([
-                    'policy', 'accident.media', 'accident.thirdParty', 'accident.media.file',
-                    'items.type', 'user'
-                ])->paginate(),
-                ClaimResource::class
-            )
-        );
+        return  $query;
     }
 }
