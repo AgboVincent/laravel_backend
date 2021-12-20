0cb1bd6cb078b445d35fae09dd3361161bf97e0f
John Flash <Johngood4real@yahoo.co.in>
2021-12-20 06:09:16 +0100

    AP-89 Fix list-query filter bug when multiple active access rights present for a baloon user
    It turns out multiple access rights are executed using a 'AND' query whereas they needed to be using 'OR'. Modify the ApplyAccessRightsToListQuery helper to ensure the queries are built only when needed and combined using an 'OR'

--- app/Actions/Integrations/Baloon/Helpers/ApplyAccessRightsToListQuery.php
+++ app/Actions/Integrations/Baloon/Helpers/ApplyAccessRightsToListQuery.php
@@ -2,10 +2,11 @@
 
 namespace App\Actions\Integrations\Baloon\Helpers;
 
+use App\DTOs\Integrations\Baloon\AccessRight;
 use App\DTOs\Integrations\Baloon\AccessRightCollection;
 use App\Helpers\Integrations\Baloon;
 use App\Models\User;
-use Illuminate\Database\Query\Builder;
+use Illuminate\Support\Facades\Log;
 use Lorisleiva\Actions\Concerns\AsObject;
 
 class ApplyAccessRightsToListQuery
@@ -22,40 +23,51 @@ class ApplyAccessRightsToListQuery
         return $query;
     }
 
+    //TODO: reduce complexity
     protected function buildFilters(AccessRightCollection $accessRights, $query){
 
-        foreach ($accessRights as $accessRight){
-            if(!in_array($accessRight->accessRightCode,['READ_CLAIM','MANAGE_CLAIM'])){
-                continue;
+        $query->where(function ($query) use ($accessRights){
+            $query->whereRaw('1=1');
+
+            foreach ($accessRights as $accessRight){
+                if(
+                    !in_array($accessRight->accessRightCode,['READ_CLAIM','MANAGE_CLAIM']) ||
+                    (!$accessRight->compagnies && !$accessRight->reseaux && !$accessRight->acteursCommerciaux)
+                ){
+                    Log::info("Skipping access right",['right'=>$accessRight]);
+                    continue;
+                }
+
+                $query->orWhere(function($query) use ($accessRight){
+                    if($accessRight->compagnies){
+                        $insurerIds = Baloon::getInsurerIdsForCompagnies($accessRight->compagnies);
+
+                        $query->whereHas('insurer',function ($insurers) use ($insurerIds){
+                            $insurers->whereIn('insurers.id',$insurerIds);
+                        });
+                    }
+
+                    if(count($accessRight->reseaux)){
+                        $query->whereHas('metas',function ($metas) use ($accessRight){
+                            $metas->where('name',Baloon::META_KEY_RESEAU_ID)
+                                ->whereIn('value',$accessRight->reseaux);
+                        });
+                    }
+
+                    if(count($accessRight->acteursCommerciaux)){
+                        $query->whereHas('metas',function ($metas) use ($accessRight){
+                            $metas->where('name',Baloon::META_KEY_ACTEUR_COMMERCIAL_ID)
+                                ->whereIn('value',$accessRight->acteursCommerciaux);
+                        });
+                    }
+                });
+
             }
-
-            $query->where(function($query) use ($accessRight){
-                if(count($accessRight->compagnies)){
-                    $insurerIds = Baloon::getInsurerIdsForCompagnies($accessRight->compagnies);
-
-                    $query->whereHas('insurer',function ($insurers) use ($insurerIds){
-                        $insurers->whereIn('insurers.id',$insurerIds);
-                    });
-                }
-
-                if(count($accessRight->reseaux)){
-                    $query->whereHas('metas',function ($metas) use ($accessRight){
-                        $metas->where('name',Baloon::META_KEY_RESEAU_ID)
-                            ->whereIn('value',$accessRight->reseaux);
-                    });
-                }
-
-                if(count($accessRight->acteursCommerciaux)){
-                    $query->whereHas('metas',function ($metas) use ($accessRight){
-                        $metas->where('name',Baloon::META_KEY_ACTEUR_COMMERCIAL_ID)
-                            ->whereIn('value',$accessRight->acteursCommerciaux);
-                    });
-                }
-            });
-
-        }
+        });
 
         return $query;
     }
+
+
 
 }
