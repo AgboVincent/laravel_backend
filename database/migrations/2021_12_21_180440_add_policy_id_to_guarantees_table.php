b890d4bbe0031b2c349f40df64b1dddbb8806b2c
Emeka Adirije <emeka.a@curacel.ai>
2021-12-21 21:23:11 +0100

    AP-102: run migrations

---
+++ database/migrations/2021_12_21_180440_add_policy_id_to_guarantees_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use App\Models\Guarantee;
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class AddPolicyIdToGuaranteesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::table('guarantees', function (Blueprint $table) {
+            $table->foreignId('policy_id')->after('guarantee_type_id')->nullable()->constrained();
+            $table->dropColumn('claim_id');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::table('guarantees', function (Blueprint $table) {
+            $table->unsignedBigInteger('claim_id')->after('guarantee_type_id');
+            $table->dropConstrainedForeignId('policy_id');
+        });
+    }
+}
